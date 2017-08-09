bf22b06450283606aab192ec0e8f58ce6a8dcbbb
sy72sdf <120260347@qq.com>
2017-08-09 17:25:31 +0800

    Initial commit

---
+++ js/yancao.js
@@ -0,0 +1,47 @@
+$(function(){
+	
+	var tanchu = setTimeout(function(){
+		$(".tanchu").show(0);
+		clearTimeout(tanchu);
+	},5000);
+	
+	$("#closetanchu").click(function(){
+		$(".tanchu").hide(0,function(){
+			var tanchu2 = setInterval(function(){
+				$(".tanchu").show(0);
+				clearInterval(tanchu2);
+			},20000)
+		});
+	})
+	
+	for(var i=0;i<$("#ul").children().length;i++){
+		$('#ul').children().children().eq(i).mouseover(function(){
+			$(this).css("color","red");
+			$(this).parent().css("border-color","red");
+			$(this).parent().css("background","url(img/img-yancao/dian2.jpg) no-repeat 0 24px")
+		});
+		$('#ul').children().children().eq(i).mouseout(function(){
+			$(this).css("color","#333333");
+			$(this).parent().css("border-color","#FFFFFF");
+			$(this).parent().css("background","url(img/img-yancao/dian1.jpg) no-repeat 0 24px")
+		});
+	}
+	
+	$("#close").mouseover(function(){
+		$(this).css("cursor","pointer");
+	});
+	$("#open").mouseover(function(){
+		$(this).css("cursor","pointer");
+	});
+	$("#close").click(function(){
+		$(".cebianlan2").fadeOut(500,function(){
+			$(".cebianlan1").fadeIn(500);
+		});
+	});
+	$("#open").click(function(){
+		$(".cebianlan1").fadeOut(500,function(){
+			$(".cebianlan2").fadeIn(500);
+		});
+	})
+	
+})
